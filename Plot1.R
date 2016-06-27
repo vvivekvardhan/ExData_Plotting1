d137ab50097fcb4b50e572bc6c61f03d722db4fb
VIVEK VARDHAN <vvivekvardhan@yahoo.co.in>
2016-06-27 01:40:23 -0500

    Exploratory Data Assignment 1 //Vivek

---
+++ Plot1.R
@@ -0,0 +1,11 @@
+Bigdata <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+Bigdata$Date <- as.Date(Bigdata$Date, format="%d/%m/%Y")
+data <- subset(Bigdata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(Bigdata)
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
